db3dee0bcc46261bcd43f3b57d7fc341665606ac
= <=>
2023-09-26 18:31:13 -0500

    chore: refactor theme helpers for readability

--- src/lib/theme/apply.ts
+++ src/lib/theme/apply.ts
@@ -1,15 +1,15 @@
 import { browser } from '$app/environment'
 
 export const applyTheme = () => {
-  if (browser) {
-    const isPersistedThemeDark = localStorage?.theme === 'dark'
-    const isThemeSet = !('theme' in localStorage)
-    const prefersDark = window.matchMedia('prefers-color-scheme: dark').matches
+  if (!browser) return
 
-    if (isPersistedThemeDark || (isThemeSet && prefersDark)) {
-      document.documentElement.classList.add('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
-    }
+  const isPersistedThemeDark = localStorage?.theme === 'dark'
+  const isThemeSet = !('theme' in localStorage)
+  const prefersDark = window.matchMedia('prefers-color-scheme: dark').matches
+
+  if (isPersistedThemeDark || (isThemeSet && prefersDark)) {
+    return document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
   }
 }
--- src/lib/theme/set.ts
+++ src/lib/theme/set.ts
@@ -5,16 +5,16 @@ import { get } from 'svelte/store'
 export const setTheme = () => {
   const currentTheme = get(theme)
 
-  if (browser) {
-    if (currentTheme === 'light') {
-      theme.set('dark')
-      localStorage.setItem('theme', 'dark')
-      document.documentElement.classList.add('dark')
-    }
-    if (currentTheme === 'dark') {
-      theme.set('light')
-      localStorage.setItem('theme', 'light')
-      document.documentElement.classList.remove('dark')
-    }
+  if (!browser) return
+
+  if (currentTheme === 'light') {
+    theme.set('dark')
+    localStorage.setItem('theme', 'dark')
+    document.documentElement.classList.add('dark')
+  }
+  if (currentTheme === 'dark') {
+    theme.set('light')
+    localStorage.setItem('theme', 'light')
+    document.documentElement.classList.remove('dark')
   }
 }
