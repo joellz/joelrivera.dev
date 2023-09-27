9ead1f1038330ce84be25da4035004ebce781e20
= <=>
2023-09-27 17:09:11 -0500

    chore: update theme switch

--- src/lib/components/ThemeSwitch.svelte
+++ src/lib/components/ThemeSwitch.svelte
@@ -1,15 +1,15 @@
 <script lang='ts'>
   import { setTheme } from '$lib/theme/set'
-  import { applyTheme } from '$lib/theme/apply'
   import { theme } from '$lib/theme/state'
 
   import MoonIcon from './icons/MoonIcon.svelte'
   import SunIcon from './icons/SunIcon.svelte'
-
-  applyTheme()
 </script>
 
-<button class='w-5 h-5 rounded-full flex items-center justify-center cursor-pointer focus:outline-dotted dark:focus:outline-white/50 focus:outline-black p-5' on:click={setTheme}>
+<button
+  class='w-5 h-5 flex items-center justify-center cursor-pointer focus:outline-dotted dark:focus:outline-white/50 focus:outline-black'
+  on:click={setTheme}
+  aria-label='Toggle dark mode'>
   {#if $theme === 'dark'}
     <SunIcon />
     {:else}
