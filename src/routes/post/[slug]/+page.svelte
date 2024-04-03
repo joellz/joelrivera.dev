2dcda227274d6e1d1e2c44c5a768db5002ef0a0a
joelriveradev <joelriveradev@gmail.com>
2024-04-02 21:33:51 -0500

    feat: update prose styles for post markdown

--- src/routes/post/[slug]/+page.svelte
+++ src/routes/post/[slug]/+page.svelte
@@ -22,7 +22,7 @@
   </p>
 
   <main
-    class="max-w-lg mt-10 pb-14 lg:mt-14 prose prose-p:text-green-50 prose-p:antialiased prose-a:text-white/50 prose-a:focus:outline-dotted prose-a:focus:outline-white/50"
+    class="max-w-lg mt-10 pb-14 lg:mt-14 prose prose-h1:text-white prose-h1:mt-12 prose-h1:font-semibold prose-h1:mb-0 prose-code:text-white/50 prose-h2:text-white prose-h2:mb-2 prose-pre:bg-white/5 prose-h3:text-white/50 prose-h3:mb-2 prose-ol:text-green-50 prose-p:text-green-50 prose-p:antialiased prose-a:text-white/50 prose-a:focus:outline-dotted prose-a:focus:outline-white/50"
   >
     {@html marked(body)}
   </main>
