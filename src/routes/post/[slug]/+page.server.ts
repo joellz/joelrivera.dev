9f4b3d2eefec26351a7729ae67af7e9e655ecf10
joelriveradev <joelriveradev@gmail.com>
2024-04-02 21:32:43 -0500

    fix: return entries from posts/[slug] for prerendering in prod

--- src/routes/post/[slug]/+page.server.ts
+++ src/routes/post/[slug]/+page.server.ts
@@ -1,9 +1,14 @@
 import type { Post } from '$lib/types/generated/graphql.js'
 import { hygraph } from '$lib/hygraph/index.js'
-import { GET_POST } from '$lib/hygraph/queries.js'
-
-export const prerender = true
+import { GET_POSTS, GET_POST } from '$lib/hygraph/queries.js'
 
 export const load = async ({ params: { slug } }) => {
   return await hygraph.request<{ post: Post }>(GET_POST, { slug })
 }
+
+export const entries = async () => {
+  const { posts } = await hygraph.request<{ posts: Array<Post> }>(GET_POSTS)
+  return posts.map(({ slug }) => ({ slug }))
+}
+
+export const prerender = true
