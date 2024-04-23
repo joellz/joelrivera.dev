53050b61b7e7843e7c2ea2c7c131429307d29cf9
joelriveradev <joelriveradev@gmail.com>
2024-04-23 12:16:36 -0500

    fix: remove prerendering

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,8 +2,6 @@ import type { AllDataResponse } from '$lib/types'
 import { hygraph } from '$lib/hygraph'
 import { GET_ALL_DATA } from '$lib/hygraph/queries'
 
-export const prerender = true
-
 export const load = async () => {
   return await hygraph.request<AllDataResponse>(GET_ALL_DATA)
 }
